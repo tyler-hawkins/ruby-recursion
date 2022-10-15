3a117cb7b69defb80dd8a161a7aa904212e87ab1
tyler-hawkins <udlr121@gmail.com>
2022-10-14 22:14:14 -0400

    Formatting

--- recursion.rb
+++ recursion.rb
@@ -2,10 +2,14 @@ def fibs(n)
 	arr = [0, 1]
 	i = 1
 	(n - arr.length()).times do
-		arr << arr[i] + arr[i-1]
-		i+=1
+		arr << arr[i] + arr[i - 1]
+		i += 1
 	end
 	arr
+end
+
+def fibs_rec(n)
+
 end
 
 p fibs(8)
